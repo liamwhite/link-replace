c84761aa3712a5f1790608e79e6ddb7d7cb20708
Liam P. White <inkscapebrony@gmail.com>
2017-06-27 21:09:14 -0400

    Factor out string replacement function copied from SO to its own file

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 all: link-replace
-link-replace.o:
+link-replace: link-replace.o str-replace.o
+link-replace.o: link-replace.c str-replace.h
+str-replace.o: str-replace.c str-replace.h
---
+++ str-replace.h
@@ -0,0 +1,7 @@
+#ifndef STR_REPLACE_H_INCLUDED
+#define STR_REPLACE_H_INCLUDED
+
+// You must free the result if result is non-NULL.
+char *str_replace(char *orig, char *rep, char *with);
+
+#endif
